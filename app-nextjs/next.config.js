4782ef69f7a15dc36d6a1f50174eb8e715ae14a6
antony <antony.suresh@gmail.com>
2026-09-18 21:47:40 +0530

    Revert "refactor: consolidate qtblocks as static desktop app"
    This reverts commit 1f7e85c03d873add3551a279bc45d22db1d91ade.

--- app-nextjs/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  outputFileTracingRoot: __dirname,
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
